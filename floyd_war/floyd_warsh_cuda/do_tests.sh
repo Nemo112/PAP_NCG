ecbd4db564da983eee9b4ce980760917cf337289
nemo <nemo@nemor.cz>
2016-05-02 19:11:28 +0200

    Added makefiles and tested O3

--- floyd_war/floyd_warsh_cuda/do_tests.sh
+++ floyd_war/floyd_warsh_cuda/do_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-i=1024;
+i=1;
 
 while [[ $i -gt 0 ]]; do
 	sed -i -e "s/define BLOCK_SIZE 512/define BLOCK_SIZE $i/g" floyd.cu;
-	nvcc floyd.cu -std=c++11 -o try;
+	nvcc floyd.cu -std=c++11 -o -O3 try;
 	sed -i -e "s/define BLOCK_SIZE $i/define BLOCK_SIZE 512/g" floyd.cu;
 	./try ../../test_data/test4000x9.txt.rnd 4 output.txt > logs_gtx_650_Ti/log$i.txt;
 	i=$(($i-1));
